08ec53d6ed390bf1aad076f03584dda6c4b91960
Om Shahane <71377782+shahane806@users.noreply.github.com>
2023-11-27 21:10:24 +0530

    Update API.js

--- src/Api/API.js
+++ src/Api/API.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-export const API = axios.create({baseURL:"http://localhost:5000/"});
+export const API = axios.create({baseURL:"https://railway.app/project/e585e30f-3275-44d1-9673-6e1e87543a5f/service/a116771e-0ab8-4160-9714-aca6bad17b11/"});
 
 export const SendMessage = (data)=> API.post("/message",data);

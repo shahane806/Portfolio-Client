dc2f5e780bf0abdba9491a15246d2e969c0625a6
om.p.shahane@gmail.com <om.p.shahane@gmail.com>
2023-11-27 21:45:39 +0530

    commit

--- src/Api/API.js
+++ src/Api/API.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-export const API = axios.create({baseURL:"https://portfolio-server-production.up.railway.app/"});
+export const API = axios.create({baseURL:"https://localhost:3000/"});
 
 export const SendMessage = (data)=> API.post("/message",data);
